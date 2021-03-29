b97bbb57c9f538eb908c44fc0c6ce3c99ee9de30
alfonso-torres <alfonso_sepul@hotmail.com>
2021-03-29 14:38:44 +0100

    try except raise and finally added.

--- orders.py
+++ orders.py
@@ -1,2 +1,19 @@
 # Dealing with files and error/exception handling
 
+# There is a built in method in python called open("")
+# file = open("orders.text") # Looks for the file called orders.text
+
+try:
+    file = open("orders.text")
+    print("File is found.")
+except FileNotFoundError as errmsg:
+    print("The above block of code was not executed." + str(errmsg))
+    #raise
+finally:
+    print("Your task is to read the data and display it as a list.")
+
+# Second iteration:
+# Your task is to read the data and display it as a list
+
+# 3rd iteration
+# Create a function to the same job DRY

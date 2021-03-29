6546fe903b2e8892a67dc87ee8cf49d1845dd7dc
alfonso-torres <alfonso_sepul@hotmail.com>
2021-03-29 18:56:58 +0100

    try except raise and finally done with examples. README updated.

--- README.md
+++ README.md
@@ -1,24 +1,30 @@
 # Dealing with files and Exception Handling in Python
 
 ## We have `try`, `except`, `raise` and `finally` as our code block to handle the error or an exceptions
+- `try`: attempt to run the code. Lets you test a block of code for errors.
+- `except`: execute if ErrorType is thrown. Lets you handle the error.
+- `finally`: executes after the try and except no matter the outcome. Lets you execute code, regardless of the result of the try- and except blocks.
+- `raise`: manually raises an error. As a developer you can choose to throw an exception if a condition occurs. ErrorType("The message you want to show.")
 
 ### To understand the concept easily:
 - Each block of code works as an if, elif, else block.
 ````python
 # There is a built in method in python called open("")
-# file = open("orders.text") # Looks for the file called orders.text
 
+# Try to open a file. This file does exist.
 try:
     file = open("orders.text")
     print("File is found.")
 except FileNotFoundError as errmsg:
-    print("The above block of code was not executed." + str(errmsg))
+    print("File not found." + str(errmsg))
     #raise
 finally:
-    print("Your task is to read the data and display it as a list.")
+    print("After opening, your task is to read the data and display it as a list.")
 ````
 - Please ensure to create an orders.text if does not exist.
-- Different modes to do with a file:
+  
+### File creation and modification
+- Different modes to operate with a file:
 
 | Mode |Description|
 | :----: |:---- |
@@ -30,7 +36,46 @@ finally:
 |'b' |This opens in binary mode.
 |'+' |This will open a file for reading and writing (updating)|
 
-
-
-
 It is worth noting that the `+` operator can be used with
+
+- Example 1 - Read data from a file and print it:
+````python
+def open_read_display(name):
+    try:
+        file = open(name)
+        print("File is found and opened.")
+    except FileNotFoundError as errmsg:
+        print("File not found." + str(errmsg))
+        raise
+    finally:
+        print("Here is the text...")
+        for line in file.readlines():
+            print(line.rstrip('\n'))
+        file.close()
+
+open_read_display("orders.text")
+````
+- Example 2 - Write an additional line to the file:
+````python
+def write_file(name, item):
+    try:
+        file = open(name, "w")
+        print("File is found and we will process to write.")
+    except FileNotFoundError as errmsg:
+        print("File not found." + str(errmsg))
+        raise
+    finally:
+        file.write(item + "\n")
+        print("List of items updated.")
+        file.close()
+
+write_file("orders.text", "Sandwich")
+````
+### CRUD
+
+CRUD is the acronym for "Create, Read, Update and Delete", which is used to refer to the basic functions in databases or the persistence layer in a software. Basically, the CRUD Operations in Python is written in python programming language and MySQL database.
+
+- `create`: 
+- `read`:
+- `update`:
+- `delete`:
--- orders.py
+++ orders.py
@@ -3,17 +3,83 @@
 # There is a built in method in python called open("")
 # file = open("orders.text") # Looks for the file called orders.text
 
-try:
-    file = open("orders.text")
-    print("File is found.")
-except FileNotFoundError as errmsg:
-    print("The above block of code was not executed." + str(errmsg))
-    #raise
-finally:
-    print("Your task is to read the data and display it as a list.")
+# Open a file that exists
+#try:
+#    file = open("orders.text")
+#    print("File is found.")
+#except FileNotFoundError as errmsg:
+#    print("The above block of code was not executed." + str(errmsg))
+#    #raise
+#finally:
+#    print("Your task is to read the data and display it as a list.")
+
+# Open a file that does not exist
+#    file = open("ordeeeers.text")
+#    print("File is found.")
+#except FileNotFoundError as errmsg:
+#    print("File not found." + str(errmsg))
+#    #raise
+#finally:
+#    print("Your task is to read the data and display it as a list.")
 
 # Second iteration:
 # Your task is to read the data and display it as a list
+#file1 = open("orders.text")
+#print(file1.read())
+#file1.close()
 
 # 3rd iteration
-# Create a function to the same job DRY
+# Create a function to read the data and display it as a list (to the same job as before) DRY
+def open_read_display(name):
+    try:
+        file = open(name) # Open the file
+        print("File is found and opened.")
+    except FileNotFoundError as errmsg:
+        print("File not found." + str(errmsg))
+        raise
+    finally:
+        print("Here is the text...")
+        for line in file.readlines():
+            print(line.rstrip('\n'))
+        file.close()
+
+open_read_display("orders.text")
+
+# -----GOOD EXAMPLE-----
+#def open_using_with_and_print(file):
+#    try:
+#        with open(file, "r") as file:
+#            for line in file.readlines():
+#                print(line.rstrip('\n'))
+#    except FileNotFoundError:
+#        print("file cannot be found or directory is incorrect, please check the details provided")
+#        raise
+#    finally:
+#        print("\nPlease chose the items from the list  and enjoy your HAPPY MEAL")
+#open_using_with_and_print("orders.text")
+
+# Create a function to write an additional line to orders.text
+def write_file(name, item):
+    try:
+        file = open(name, "w") # Open the file with the mode of written
+        print("File is found and we will process to write.")
+    except FileNotFoundError as errmsg:
+        print("File not found." + str(errmsg))
+        raise
+    finally:
+        file.write(item + "\n")
+        print("List of items updated.")
+        file.close()
+
+write_file("orders.text", "Sandwich")
+
+# -----GOOD EXAMPLE-----
+#def write_to_file(file, order_item):
+#    try:
+#        with open(file, "w") as file:
+#            file.write(order_item + '\n')
+#    except FileNotFoundError:
+#        print("file cannot be found or directory is incorrect, please check the details provided")
+#        raise
+
+#write_to_file("order.txt", "Ice Cream")
